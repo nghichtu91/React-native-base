07c5a3af02edc88faa4ef366469ff23610971291
nghichtu91 <nhatthanh5891@gmail.com>
2021-07-26 19:51:54 +0700

    add react-native-paper package

--- ios/TrekkingApp/AppDelegate.m
+++ ios/TrekkingApp/AppDelegate.m
@@ -1,4 +1,7 @@
 #import "AppDelegate.h"
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
 #if RCT_DEV
 #import <React/RCTDevLoadingView.h>
 #endif
@@ -31,6 +34,9 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+[AppCenterReactNative register];
+[AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+[AppCenterReactNativeCrashes registerWithAutomaticProcessing];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
